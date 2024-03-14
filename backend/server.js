923d02d2a2cf9a237b655f67f1c0df6f633baf36
Shivam  Mishra <shivammmishra7@gmail.com>
2024-03-14 16:12:52 +0530

    live endpoint

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const server = app.listen(PORT, console.log(`Server started on port ${PORT}`.yel
 const io = require("socket.io")(server, {
   pingTimeout: 120000,
   cors: {
-    origin: "http://localhost:3000", //development    
+    origin: "https://taktik.onrender.com", //development    
     credentials: true,
   },
 });
